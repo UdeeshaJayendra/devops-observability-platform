2b10904cf4b4814f609dd5bcd886b6681e1469a2
Udeesha <rathnayakaudeesha@gmail.com>
2026-09-19 23:47:20 +0530

    feat: add distributed tracing with OpenTelemetry and Tempo

---
+++ app/src/tracing.js
@@ -0,0 +1,16 @@
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+
+const traceExporter = new OTLPTraceExporter({
+  url: "http://tempo:4318/v1/traces"
+});
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start();
+
+console.log("OpenTelemetry tracing initialized");
